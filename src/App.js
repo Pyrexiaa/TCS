15933c04f9f22e632a6a1b533bb2727fb579e09a
Kian Khoon <chiew256@gmail.com>
2022-12-29 20:39:56 +0800

    remove redundant browser router in App component

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import './App.css';
 import Navbar from './components/navbar/navbar';
 import FirstPage from './components/FirstPage/FirstPage';
 import AboutMuseum from './components/AboutMuseumPage/AboutMuseum';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<FirstPage />} />
-          <Route path="/aboutMuseum" element={<AboutMuseum />} />
-        </Routes>
-      </BrowserRouter>
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<FirstPage />} />
+                <Route path="/aboutMuseum" element={<AboutMuseum />} />
+            </Routes>
 
-    </>
+        </>
 
-  )
+    );
 }
 
 export default App;
