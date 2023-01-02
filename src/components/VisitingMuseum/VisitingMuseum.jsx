a4d3699c1c1cc623c8af0fb06214ed992304a19f
Jia Yu Lim <U2102820@siswa.um.edu.my>
2023-01-02 16:49:41 +0800

    Edited

--- src/components/VisitingMuseum/VisitingMuseum.jsx
+++ src/components/VisitingMuseum/VisitingMuseum.jsx
@@ -12,17 +12,17 @@ const VisitingMuseum = () => {
             <div className='underline-text'></div>
           </span>
           <p className='small-description'>What's in the museum</p>
-          <div class="aboutVisiting-content">
+          <div className="aboutVisiting-content">
             <ul>
               <li>
                 <span>
-                  <p class="small-white-text">
+                  <p className="small-white-text">
                     Task
                   </p>
                 </span>
               </li>
               <li>
-                <p class="medium-blue-text">
+                <p className="medium-blue-text">
                   Collect, preserve and spread quality
                   information and materials professionally
                   which are related to historical heritage
@@ -33,13 +33,13 @@ const VisitingMuseum = () => {
             <ul>
               <li>
                 <span>
-                  <p class="small-white-text">
+                  <p className="small-white-text">
                     Kampung Duyong
                   </p>
                 </span>
               </li>
               <li>
-                <p class="medium-blue-text">
+                <p className="medium-blue-text">
                   Anyway, he is said to be from Malacca. And his village in Kampung Duyong which is said to have existed for over 600 years.
                 </p>
               </li>
@@ -47,13 +47,13 @@ const VisitingMuseum = () => {
             <ul>
               <li>
                 <span>
-                  <p class="small-white-text">
+                  <p className="small-white-text">
                     Hang Tuah Village
                   </p>
                 </span>
               </li>
               <li>
-                <p class="medium-blue-text">
+                <p className="medium-blue-text">
                   And to remember and recall Hang Tuah's heroism, the state of Malacca finally decided to develop the Hang Tuah Village complex. The size is 40 acres, and the cost is over 130 million.
                   <br></br>
                   <br></br>
--- src/components/displayImage/DisplayImage.jsx
+++ src/components/displayImage/DisplayImage.jsx
@@ -10,32 +10,32 @@ const DisplayImage = () => {
                     <div className='visiting-image'>
                         <img src={visitingImage}></img>
                     </div>
-                    <div class="about-display-content">
+                    <div className="about-display-content">
                         <ul>
                             <li>
                                 <span>
-                                    <p class="small-white-text">
+                                    <p className="small-white-text">
                                         You can learn a little bit more about Admiral Hang Tuah's life here.
                                     </p>
                                 </span>
                             </li>
                             <li>
                                 <span>
-                                    <p class="small-white-text">
+                                    <p className="small-white-text">
                                         Malacca Malay houses can also be found in this location. Additionally, each home has the names of one of Hang Tuah's fellow soldiers, such as Hang Jebat, who battled alongside him against human defamation.
                                     </p>
                                 </span>
                             </li>
                             <li>
                                 <span>
-                                    <p class="small-white-text">
+                                    <p className="small-white-text">
                                         You can enter these Malay houses every day (except the Hang Jebat and Kasturi houses which are closed every Friday).
                                     </p>
                                 </span>
                             </li>
                             <li>
                                 <span>
-                                    <p class="small-white-text">
+                                    <p className="small-white-text">
                                         In this area there is also a museum with historical artifacts of the development of Malacca.
                                     </p>
                                 </span>
