78b4f86dc420ea1f5bd850e1d989a304622dbbc6
Jia Yu Lim <U2102820@siswa.um.edu.my>
2023-01-12 21:19:41 +0800

    Finalised Image

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Gallery.css'
 import GalleryHeader from '../GalleryHeader/GalleryHeader'
 import Footer from '../footer/Footer'
-import { gallery1, gallery2, gallery3, gallery4, gallery5, gallery6, gallery7, gallery8 } from '../../assets'
+import { gallery1, gallery2, gallery3, gallery4, gallery5, gallery6, gallery7, gallery8, gallery9, gallery10 } from '../../assets'
 
 const Gallery = () => {
     return (
@@ -19,7 +19,7 @@ const Gallery = () => {
                                         <img src={gallery1} alt="Malacca History" />
                                         <div className="description-box">
                                             <div className="description-caption">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                                <p>The Hang Tuah Centre, which is said to be located on the land that Hang Tuah honed his silat skills, also sites five houses of the five famous warriors during Sultan Mansur Shah. Each house is equipped with a silat court for silat training and cultural activities.</p>
                                             </div>
                                         </div>
                                     </div>
@@ -51,6 +51,15 @@ const Gallery = () => {
                                     </div>
                                     <div className="image-container">
                                         <img src={gallery8} alt="Wallpaper Pattern" />
+                                    </div>
+                                </div>
+
+                                <div className="gallery-row-2">
+                                    <div className="image-container">
+                                        <img src={gallery9} alt="Baling Tin" />
+                                    </div>
+                                    <div className="image-container">
+                                        <img src={gallery10} alt="Spears" />
                                     </div>
                                 </div>
                             </div>
